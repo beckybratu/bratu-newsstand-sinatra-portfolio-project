dc0428753d4219ca82f7233c2b8459aecce3012e
Rebecca Bratu <beckybratu@gmail.com>
2017-12-19 03:42:04 +0000

    Started building application controller.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
 
   has_many :subscriptions
   has_many :titles, :through => :subscriptions
+  has_many :categories, :through => :titles
   has_secure_password
 
-end 
+end
